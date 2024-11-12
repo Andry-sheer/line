773db3b9aaddb5fdd5e2668e655f4544ff658ed6
Andry <andry.sheremetew@gmail.com>
2024-11-12 16:38:29 +0200

    deleted reME

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 
-
 const App = () => {
   return (
-    <h1 className="text-3xl text-lime-400">App</h1>
+    <h1 className="text-3xl text-lime-400 text-center">App</h1>
   )
 }
 
-export default App
+export default App;
