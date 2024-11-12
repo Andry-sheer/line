494f412b73e34cc47c49a40cc6b6a88346b84a63
Andry <andry.sheremetew@gmail.com>
2024-11-12 21:46:45 +0200

    create Pages + Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import SingIn from './pages/SingIn';
+import SingUp from './pages/SingUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-lime-400 text-center">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<Error/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/sing-in' element={<SingIn/>} />
+        <Route path='/sing-up' element={<SingUp/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/projects' element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
